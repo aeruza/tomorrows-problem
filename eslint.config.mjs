0b7d00afc3b9523533e26f29eb1bf8d0604ecf24
Sean <118966410+aeruza@users.noreply.github.com>
2026-06-18 00:26:33 +0800

    fix: revert mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,18 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const eslintConfig = defineConfig([
+    ...nextVitals, 
+    ...nextTs,
 
-const compat = new FlatCompat({ baseDirectory: __dirname });
+    globalIgnores([
+        // Default ignores of eslint-config-next
+        ".next/**",
+        "out/**",
+        "build/**",
+        "next-env.d.ts",
+    ]),
+]);
 
-export default [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
-    {
-        ignores: [
-            ".next/**",
-            "out/**",
-            "build/**",
-            "next-env.d.ts",
-        ],
-    },
-];
+export default eslintConfig;
