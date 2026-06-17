a31313ffdafc3f66afc19879038cc2377a146c10
Sean <118966410+aeruza@users.noreply.github.com>
2026-06-18 00:25:30 +0800

    fix: mjs fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,20 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-const eslintConfig = defineConfig([
-    ...(Array.isArray(nextVitals) ? nextVitals : [nextVitals]),
-    ...(Array.isArray(nextTs) ? nextTs : [nextTs]),
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-    globalIgnores([
-        ".next/**",
-        "out/**",
-        "build/**",
-        "next-env.d.ts",
-    ]),
-]);
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-export default eslintConfig;
+export default [
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        ignores: [
+            ".next/**",
+            "out/**",
+            "build/**",
+            "next-env.d.ts",
+        ],
+    },
+];
